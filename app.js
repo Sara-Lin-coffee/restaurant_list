914dd36f41c4856f3022d8aadebde7d92e60d0d4
coffee.ewa@gmail.com <coffee.ewa@gmail.com>
2022-12-18 20:59:01 +0800

    add edit.handlebars

--- app.js
+++ app.js
@@ -50,6 +50,14 @@ app.get('/restaurants/:id', (req, res) =>{
   .then((restaurant) => {res.render('show', {restaurant})})
 })
 
+//render edit page for each restaurant
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => { res.render('edit', { restaurant }) })
+})
+
 app.get('/search', (req, res) =>{
   Restaurant.find()
   .lean()
